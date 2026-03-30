531aee6cd522db17464c4f256395f4ff20de4bac
betsyphilip <betsyphilip@gmail.com>
2026-03-30 12:03:21 +0530

    json import

--- file.py
+++ file.py
@@ -11,11 +11,7 @@ conn=pymongo.MongoClient("mongodb://localhost:27017/")
 # else: 
 # print("failed")'''
 
-import pymongo
 
-conn = pymongo.MongoClient("mongodb://localhost:27017/")
-db = conn["student"]
-col = db["mark2"]
 
 # List of multiple documents
 '''data = [
@@ -58,7 +54,13 @@ else:
     
 '''
 
-for i in col.find({}, {"_id": 0}):
+import pymongo
+
+conn = pymongo.MongoClient("mongodb://localhost:27017/")
+db = conn["college1"]
+col = db["studlist1"]
+
+'''for i in col.find({}, {"_id": 0}):
     total = i["mark1"] + i["mark2"] + i["mark3"]
     print("Name:", i["name"], "Total Marks:", total)
 
@@ -68,7 +70,77 @@ for i in col.find(
     ).sort([("name", -1)]):           
     print(i["name"])
 
-col.delete_one({"rollno":13})
+col.delete_one({"rollno":13})'''
 
-
+'''result = col.find(
+    {"gender": "female", "course": "MCA"},
+    {"name.fname": 1, "name.lname": 1, "mark": 1, "_id": 0}
+)
+                                                                                #1
+for doc in result:
+    print(doc)
     
+result = col.find({"course": "MCA"}).sort("mark", -1).limit(1)
+
+for doc in result:
+    print(doc)                                                                      #2
+
+
+result = col.find({"gender": "male", "grade": "A+"})
+
+for doc in result:
+    print(doc)                                                                            #3
+    
+result = col.find(
+    {"course": "Mechanical"},
+    {"name.fname": 1, "name.lname": 1, "_id": 0}
+).sort("mark", -1).limit(3)
+
+for doc in result:
+    print(doc)                                                                       #4
+    
+result = col.find(
+    {"gender": "female", "mark": {"$gt": 90}},
+    {
+        "name.fname": 1,
+        "name.lname": 1,
+        "grade": 1,
+        "mark": 1,
+        "phone": 1,
+        "_id": 0
+    }
+)
+
+for doc in result:
+    print(doc)                                                                #5
+ 
+result = col.find({"mark": {"$gt": 80, "$lt": 90}})
+
+for doc in result:
+    print(doc)                                                              #6
+
+result = col.find({"name.fname": {"$regex": "^V", "$options": "i"}})
+
+for doc in result:
+    print(doc)                                                           #7
+    
+    
+result = col.find({"address.city": "Kollam"})
+
+for doc in result:
+    print(doc)                                                   #8
+    
+result = col.find({
+    "address.city": {"$nin": ["Kollam", "Thiruvananthapuram"]}
+})
+
+for doc in result:
+    print(doc)              '''                                          #9
+    
+result = col.find({
+    "gender": "female",
+    "address.city": {"$in": ["Kollam", "Thiruvananthapuram"]}
+})
+
+for doc in result:
+    print(doc)                                                                    #10
